d6943be0cb4a94997b3c5013295d668dfa20636d
16sheep <marjutubli@gmail.com>
2019-06-18 17:28:39 +0100

    add serialize

--- build/bundle.js
+++ build/bundle.js
@@ -381,6 +381,10 @@ var _reactRedux = __webpack_require__(3);
 
 var _reactRouterConfig = __webpack_require__(1);
 
+var _serializeJavascript = __webpack_require__(19);
+
+var _serializeJavascript2 = _interopRequireDefault(_serializeJavascript);
+
 var _Routes = __webpack_require__(2);
 
 var _Routes2 = _interopRequireDefault(_Routes);
@@ -402,7 +406,7 @@ exports.default = function (req, store) {
             )
         )
     ));
-    return '<html>\n                <head>\n                    <body>\n                        <div id="root">' + content + '</div>\n                        <script>window.INITIAL_STATE = ' + JSON.stringify(store.getState()) + '</script>\n                        <script src="bundle.js"></script>\n                    </body>\n                </head>\n            <html>';
+    return '<html>\n                <head>\n                    <body>\n                        <div id="root">' + content + '</div>\n                        <script>window.INITIAL_STATE = ' + (0, _serializeJavascript2.default)(store.getState()) + '</script>\n                        <script src="bundle.js"></script>\n                    </body>\n                </head>\n            <html>';
 };
 
 /***/ }),
@@ -498,6 +502,12 @@ exports.default = function () {
             return state;
     }
 };
+
+/***/ }),
+/* 19 */
+/***/ (function(module, exports) {
+
+module.exports = require("serialize-javascript");
 
 /***/ })
 /******/ ]);
