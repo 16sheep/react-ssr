fade724be18a71b507f42a0c555a96a2a3352b67
16sheep <marjutubli@gmail.com>
2019-06-18 09:28:30 +0100

    Use import module system on server

--- build/bundle.js
+++ build/bundle.js
@@ -6917,14 +6917,26 @@ if (process.env.NODE_ENV === 'production') {
 "use strict";
 
 
-var express = __webpack_require__(52);
-var React = __webpack_require__(16);
-var renderToString = __webpack_require__(113).renderToString;
-var Home = __webpack_require__(124).default;
-var app = express();
+var _express = __webpack_require__(52);
+
+var _express2 = _interopRequireDefault(_express);
+
+var _react = __webpack_require__(16);
+
+var _react2 = _interopRequireDefault(_react);
+
+var _server = __webpack_require__(113);
+
+var _Home = __webpack_require__(124);
+
+var _Home2 = _interopRequireDefault(_Home);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
 
 app.get('/', function (req, res) {
-    var content = renderToString(React.createElement(Home, null));
+    var content = (0, _server.renderToString)(_react2.default.createElement(_Home2.default, null));
     res.send(content);
 });
 
