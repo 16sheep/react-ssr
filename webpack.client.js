8d8fb1fc05ae5c3f39fa552a2fece0ab1863a02c
16sheep <marjutubli@gmail.com>
2019-06-18 10:57:14 +0100

    Add webpack-node-externals

--- webpack.client.js
+++ webpack.client.js
@@ -5,8 +5,10 @@ const config = {
     //Tell webpack of root file of application
     entry: './src/client/client.js',
     //tell webpack where to put output file, __dirname is current working directory
-    output: {filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')}
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    }
 }
 
 module.exports = merge(baseConfig, config)
--- webpack.server.js
+++ webpack.server.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const merge = require('webpack-merge')
+const webpackNodeExternals = require("webpack-node-externals")
 const baseConfig = require('./webpack.base.js')
 const config = {
     //inform webpack we are building bundle for node, not browser
@@ -7,8 +8,11 @@ const config = {
     //Tell webpack of root file of application
     entry: './src/index.js',
     //tell webpack where to put output file, __dirname is current working directory
-    output: {filename: 'bundle.js',
-    path: path.resolve(__dirname, 'build')}
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'build')
+    },
+    externals: [webpackNodeExternals()]
 }
 
 module.exports = merge(baseConfig, config)
