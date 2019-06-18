9f6bc36b9de0dad402c97aa74c733c654311d571
16sheep <marjutubli@gmail.com>
2019-06-18 10:50:13 +0100

    Refactor startup with single script

---
+++ webpack.base.js
@@ -0,0 +1,16 @@
+module.exports = {
+    //tell webpack to run babel on every file which is javascript file, stage-0 helps run async code
+
+    module: {
+        rules: [{
+            test: /\.js?$/,
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            options:{
+                presets: [
+                    'react', 'stage-0', ['env', {targets: {browsers: 'last 2 versions'}}]
+                ]
+            }
+        }]
+    }
+}
--- webpack.client.js
+++ webpack.client.js
@@ -1,22 +1,12 @@
 const path = require('path')
-
-module.exports = {
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.base.js')
+const config = {
     //Tell webpack of root file of application
     entry: './src/client/client.js',
     //tell webpack where to put output file, __dirname is current working directory
     output: {filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')},
-    //tell webpack to run babel on every file which is javascript file, stage-0 helps run async code
-    module: {
-        rules: [{
-            test: /\.js?$/,
-            loader: 'babel-loader',
-            exclude: /node_modules/,
-            options:{
-                presets: [
-                    'react', 'stage-0', ['env', {targets: {browsers: 'last 2 versions'}}]
-                ]
-            }
-        }]
-    }
+    path: path.resolve(__dirname, 'public')}
 }
+
+module.exports = merge(baseConfig, config)
